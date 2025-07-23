23fc4b90b45b6498a2da58b01be66ec665a8eef2
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-07-23 17:02:23 -0500

    change the niche to entertainment and introduced optional parameters

---
+++ creator.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Creator(BaseModel):
+  username:str
+  display_name:str
+  bio:Optional[str]=None
+  profile_pic_url:Optional[str]=None
+  email:str
+
+creator=Creator(username="tyrumanzi",display_name="Rumanzi",email="askmehow@gmail.com")
+print(creator)
